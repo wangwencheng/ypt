367c08c8b6796abddacd7f56608d7da8c887ecbe
960075207@qq.com <273273wang>
2018-12-16 21:21:05 +0800

    关联店铺推荐代码上传

---
+++ src/main/java/com/example/ypt/web/request/RecommendRequest.java
@@ -0,0 +1,11 @@
+package com.example.ypt.web.request;
+
+import lombok.Data;
+
+@Data
+public class RecommendRequest {
+    private Long count;
+    private String fields;
+    private Long platform;
+    private Long userId;
+}
